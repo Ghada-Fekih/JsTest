ce62901d602000f06a9a01ff9ac4a901728a52aa
akrem <148336119+Ghada-Fekih@users.noreply.github.com>
2023-11-22 11:17:08 +0100

    create test js
    add html, css, js

---
+++ js/min.js
@@ -0,0 +1,48 @@
+// récuperer les valeurs des champs
+const prénom = document.querySelector('prénom');
+const mail = document.querySelector('mail');
+const tel = document.querySelector('tel');
+const error = document.querySelector('.error');
+const submit = document.querySelector('submit');
+const affiche = document.querySelector('affiche');
+
+// validation des inputs
+
+submit.addEventListener('click', (event) => {
+    if (prénom.value == ""){
+        error.innerHTML = `<span>nom est vide</span> `;
+        return;
+    }
+    if (!/^[a-zA-Z]+$/.test(prénom)) {
+        error.innerHTML = `<span>"le prénom doit contenir seulement des lettres"</span> `;
+        return ;}
+
+    else {error.innerHTML = "";}
+    if (mail.value == ""){
+      error.innerHTML = `<span>mail est vide</span> `;
+      return;
+  }
+  if (!/^[a-zA-Z0-9]+@[a-zA-Z]+[?:\.com .fr .tn]$/) {
+    error.innerHTML = `<span>"mail doit contenir @ et .com .fr .tn"</span> `; 
+    return;
+}
+  else {error.innerHTML = "";}
+  if (tel.value == ""){
+      error.innerHTML = `<span>tel est vide</span> `;
+      return;
+  }
+  if (!/^[0-9]+$/.test(tel)){
+      error.innerHTML=`tel doit etre des numero `;
+  }
+  else {error.innerHTML = "";}
+  const longeurTel = 8;
+   
+   if (tel !== longeurTel) {
+    error.innerHTML=`"tel doit contenir 8 chiffres" `;
+    return;
+   }
+   return null;
+   // afficher le resultat dans le champs affiche
+
+})
+  
